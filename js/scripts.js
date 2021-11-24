debc66d6e19b32fccfc148a4963b0da04b91be18
wallace azevedo <wallaceazevedo17@gmail.com>
2021-11-23 22:16:49 -0300

    new method
    metodo includes para verificar se o estado existe no array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // array de stados para sabe se eles existem usando o metodo includes e sem precisar usar while e for
 // link da aula https://www.youtube.com/watch?v=mkQaDD9rc28
-let estados= [ "Minas","Goias", "São Paulo"];
+let estados = [ "Minas","Goias", "São Paulo"];
 
 console.log(estados.includes("São Paulo"));
 
@@ -18,3 +18,11 @@ if(estados.includes("join ville")){
 } else {
     console.log("nâo, join ville nâo existe ");
 }
+
+//metodo includes para verificar se o estado existe no array
+let brasil = ["Acre"];
+const isAcreIncluded = brasil.includes("Acre");
+const acreMessage = isAcreIncluded
+    ? 'Acre está incluido.'
+    : 'Acre nâo foi incluido =/'
+console.log(acreMessage);
