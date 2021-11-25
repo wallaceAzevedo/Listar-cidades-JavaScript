5ab073512574ecc42d9f961f136665ba5ce27e97
wallace azevedo <wallaceazevedo17@gmail.com>
2021-11-25 07:00:31 -0300

    update javascript

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,35 @@ console.log(fruits.indexOf('banana')); // -1 (false)
 // verificando se ela existe e a ordem e igual
 console.log(fruits.indexOf('pera', 1)); // 1    (true)
 console.log(fruits.indexOf('pera', 4)); // -1   (false)
+
+
+
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+total = 0;
+
+function somar(item, indice, numeros){
+    total += item;
+    numeros[indice] = total;
+}
+numeros.forEach(somar);
+console.log(numeros);
+
+
+function temHabilidade(skills) {
+    for (const item of skills) if (item == "JavaScript") return true;
+    return false;
+}
+let skills = ["JavaScript", "ReactJS", "Flutter","React Native"];
+var resultado = temHabilidade(skills);
+console.log(resultado);
+
+
+for (let index = 0; index < skills.length; index++) {
+    const element = skills[index];
+    console.log(element);
+    console.log(index);
+    console.log()
+    if(element == "React Native"){
+        console.log('')
+    }
+}
